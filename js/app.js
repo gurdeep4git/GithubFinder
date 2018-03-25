32a8244c571879a6d8ffbdd9ad52f32f0def6771
Gurdeep Singh <gurdeep.singh@prmtechnologies.com>
2018-03-25 19:04:25 +0530

    Add functionality code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-app.js
+const github = new Github();
+const ui = new UI();
+
+const searchUser = document.getElementById("searchUser");
+
+searchUser.addEventListener('keyup', (e) => {
+    const userVal = e.target.value;
+    if(userVal!=""){
+        // Make http call
+        github.getUser(userVal)
+        .then((data) => {
+            if(data.profile.message==="Not Found"){
+                // show alert not found
+                ui.showAlert("User not found",'alert alert-danger');
+            }
+            else{
+                // show profile
+                ui.showProfile(data.profile);
+                //ui.showRepos(data.repos);
+            }
+        });
+    }
+    else{
+        // clear profile
+        ui.clearProfile();
+    }
+});
--- js/github.js
+++ js/github.js
@@ -1,1 +1,24 @@
-github.js
+class Github{
+    //Constructor
+    constructor(){
+        this.clien_Id = '8fd8900982fcabbf6a82';
+        this.client_secret = '7307483a2fbb2d8b10b35e2816650095054d5d67';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+    //Methods
+    async getUser(username){
+        const profileResponse = await fetch(`https://api.github.com/users/${username}?client_id=${this.clien_Id}&client_secret=${this.client_secret}`);
+        
+        const repoResponse = await fetch(`https://api.github.com/users/${username}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        
+        const repos = await repoResponse.json();
+        
+        return{
+            profile:profile,
+            repos:repos
+        }
+    }
+}
